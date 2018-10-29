ec45b5b51ddf86bdcaaa9ce17ef857476c07e76d
BerryHang <18801776382@163.com>
2018-10-29 19:50:15 +0800

    spring boot jpa

---
+++ book-spring-boot-actual-combat-jpa/src/main/java/com/learning/spring/boot/controller/HelloWord.java
@@ -0,0 +1,21 @@
+package com.learning.spring.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author beibei.huang
+ * @Title: HelloWord
+ * @ProjectName spring-boot-learning
+ * @Description: 第一个接口
+ * @date 2018/10/23    10:30
+ */
+@RestController
+public class HelloWord {
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/learning/spring/boot/BookSpringBootActualCombatParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learning.spring.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookSpringBootActualCombatParentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
