3cdea799f73bb59d7ed4523c9b052282f74117a4
Berry Huang <beibei.huang@weimob.com>
2018-11-08 20:30:54 +0800

    修改文档内容-文件上传

---
+++ book-spring-boot-actual-combat-upload/src/main/java/com/learning/spring/boot/BookSpringBootActualCombatUploadApplication.java
@@ -0,0 +1,14 @@
+package com.learning.spring.boot;
+
+/**
+ * @author beibei.huang
+ * @Title:
+ * @ProjectName book-spring-boot-actual-combat-parent
+ * @Description: TODO
+ * @date 2018/11/8    14:35
+ */
+
+public class BookSpringBootActualCombatUploadApplication {
+
+
+}
