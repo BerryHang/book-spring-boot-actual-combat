23f6ec3d28c791c552375d9ee5e577b52a79d69d
BerryHuang <18801776382@163.com>
2018-11-13 22:15:52 +0800

    修改代码异常

---
+++ book-spring-boot-actual-combat-banner/src/main/java/com/learning/spring/boot/BookSpringBootActualCombatBannerApplication.java
@@ -0,0 +1,10 @@
+package com.learning.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+
+public class BookSpringBootActualCombatBannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSpringBootActualCombatBannerApplication.class, args);
+    }
+}
