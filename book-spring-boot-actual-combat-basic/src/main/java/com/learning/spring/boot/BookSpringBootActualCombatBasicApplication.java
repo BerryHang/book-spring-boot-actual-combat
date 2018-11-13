98a0ea97e73bc988d806c40bf889d468c01de130
Berry Huang <beibei.huang@weimob.com>
2018-11-13 17:56:55 +0800

    重构项目

---
+++ book-spring-boot-actual-combat-basic/src/main/java/com/learning/spring/boot/BookSpringBootActualCombatBasicApplication.java
@@ -0,0 +1,20 @@
+package com.learning.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author beibei.huang
+ * @Title:
+ * @ProjectName book-spring-boot-actual-combat-parent
+ * @Description: Spring boot 基础
+ * @date 2018/11/13    14:16
+ */
+@SpringBootApplication
+public class BookSpringBootActualCombatBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSpringBootActualCombatBasicApplication.class, args);
+    }
+
+}
