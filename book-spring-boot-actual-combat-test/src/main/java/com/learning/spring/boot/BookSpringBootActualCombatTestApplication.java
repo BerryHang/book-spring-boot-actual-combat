8e2c1e897db61a0e177c3b57b17348fcd87a93f3
Berry Huang <beibei.huang@weimob.com>
2019-01-18 17:48:06 +0800

    新增接口测试模块

---
+++ book-spring-boot-actual-combat-test/src/main/java/com/learning/spring/boot/BookSpringBootActualCombatTestApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSpringBootActualCombatTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSpringBootActualCombatTestApplication.class, args);
+    }
+
+}
