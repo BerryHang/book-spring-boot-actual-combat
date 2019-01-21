bb3f77987b43fe0d463a3e2bfdbab2eceb6c6d74
BerryHuang <18801776382@163.com>
2019-01-21 22:29:17 +0800

    新增书籍内容自定义banner

--- book-spring-boot-actual-combat-banner/src/main/java/com/learning/spring/boot/BookSpringBootActualCombatBannerApplication.java
+++ book-spring-boot-actual-combat-banner/src/main/java/com/learning/spring/boot/BookSpringBootActualCombatBannerApplication.java
@@ -1,7 +1,9 @@
 package com.learning.spring.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BookSpringBootActualCombatBannerApplication {
 
     public static void main(String[] args) {
