c161122c8d5f00ace0a1558889d9f1edb82b5f80
Berry Huang <beibei.huang@weimob.com>
2019-02-15 11:42:35 +0800

    修正WEB模块中与前一章不衔接内容

--- book-spring-boot-actual-combat-thymeleaf/src/main/java/com/learning/spring/boot/interceptor/LogInterceptor.java
+++ book-spring-boot-actual-combat-thymeleaf/src/main/java/com/learning/spring/boot/interceptor/LogInterceptor.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
  * @author beibei.huang
  * @Title: LogInterceptor
  * @ProjectName spring-boot-learning
- * @Description: TODO
+ * @Description: 日志拦截器
  * @date 2018/10/23    16:02
  */
 @Component
