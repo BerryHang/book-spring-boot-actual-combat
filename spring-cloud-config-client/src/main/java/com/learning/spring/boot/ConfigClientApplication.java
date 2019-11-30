281f189243413d3613971f653b017628c29e581d
Berry Huang <beibei.huang@weimob.com>
2019-11-30 17:50:43 +0800

    完善数据内容描述,添加Spring Cloud Config内容

---
+++ spring-cloud-config-client/src/main/java/com/learning/spring/boot/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.learning.spring.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Package: com.learning.spring.boot
+ * @ClassName: ConfigClientApplication
+ * @Author: beibei.huang
+ * @Description: Spring Cloud Config Client 配置
+ * @Date: 2019/11/30 17:42
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+}
